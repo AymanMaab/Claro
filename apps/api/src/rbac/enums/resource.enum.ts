fc01df1848205cd4e771d3f722db4b464cf78bb6
AymanMaab <aymonbacha@gmail.conm>
2026-06-19 23:55:48 +0500

    feat: rbac implementation, create enums

---
+++ apps/api/src/rbac/enums/resource.enum.ts
@@ -0,0 +1,11 @@
+export enum Resource {
+  ACCOUNTS = 'accounts',
+  TRANSACTIONS = 'transactions',
+  BUDGETS = 'budgets',
+  CSV_IMPORTS = 'csv_imports',
+  NOTIFICATIONS = 'notifications',
+  ANALYTICS = 'analytics',
+  USERS = 'users',
+  ROLES = 'roles',
+  PERMISSIONS = 'permissions',
+}
---
+++ apps/api/src/rbac/enums/role-group.enum.ts
@@ -0,0 +1,5 @@
+export enum RoleGroup {
+  SUPER_USER = 'SUPER_USER',
+  ADMIN = 'ADMIN',
+  MEMBER = 'MEMBER',
+}
